77c90f528b3573bb7fac7969cd0ce3009d94538e
MariaPoalelungi2003 <158208036+MariaPoalelungi2003@users.noreply.github.com>
2024-09-25 20:54:34 +0300

    problem get total1

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,24 +1,26 @@
 import prismadb from "@/lib/prismadb";
 
-export const getTotalRevenue=async (storeId:string)=>{
+export const getTotalRevenue = async (storeId: string) => {
     const paidOrders = await prismadb.order.findMany({
-        where:{
+        where: {
             storeId,
-            isPaid:true,
+            isPaid: true,
         },
-        include:{
-            orderItems:{
-                include:{
-                    product:true
-                }
-            }
-        }
+        include: {
+            orderItems: {
+                include: {
+                    product: true,
+                },
+            },
+        },
     });
-    const totalRevenue =paidOrders.reduce((total,order)=>{
-       const orderTotal=order.orderItems.reduce((orderSum,item)=>{
-          return orderSum+item.product.price.toNumber();
-       },0)
-        return total+orderTotal;
-    },0)
+
+    const totalRevenue = paidOrders.reduce((total: number, order) => {
+        const orderTotal = order.orderItems.reduce((orderSum: number, item) => {
+            return orderSum + item.product.price.toNumber();
+        }, 0);
+        return total + orderTotal;
+    }, 0);
+
     return totalRevenue;
 }
