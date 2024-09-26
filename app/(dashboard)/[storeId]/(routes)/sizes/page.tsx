a91be7600fb29dd07c009b11e49b67b7050f2d07
MariaPoalelungi2003 <158208036+MariaPoalelungi2003@users.noreply.github.com>
2024-09-26 19:58:12 +0300

    problem get total23

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,38 +1,44 @@
 import { format } from "date-fns";
-
 import prismadb from "@/lib/prismadb";
 import { SizeClient } from "./components/client";
 import { SizeColumn } from "./components/columns";
 
 const SizesPage = async ({
-    params
-}: {
-    params: {storeId: string}
+                             params,
+                         }: {
+    params: { storeId: string };
 }) => {
     const sizes = await prismadb.size.findMany({
         where: {
-            storeId: params.storeId
+            storeId: params.storeId,
         },
-
         orderBy: {
-            createdAt: 'desc'
-        }
+            createdAt: "desc",
+        },
     });
-    
-    const formattedSizes: SizeColumn[] = sizes.map(( item ) => ({
-        id: item.id,
-        name: item.name,
-        value: item.value,
-        createdAt: format(item.createdAt, "MMMM do, yyyy")
-    }))
+
+    // Adăugarea tipului explicit pentru 'item'
+    const formattedSizes: SizeColumn[] = sizes.map(
+        (item: {
+            id: string;
+            name: string;
+            value: string; // sau tipul adecvat dacă value este de un alt tip
+            createdAt: Date;
+        }) => ({
+            id: item.id,
+            name: item.name,
+            value: item.value,
+            createdAt: format(item.createdAt, "MMMM do, yyyy"),
+        })
+    );
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <SizeClient data={formattedSizes}/>
+                <SizeClient data={formattedSizes} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default SizesPage;
