d717553fe9de91a0665d5eb9e12475ea9fbd0a71
MariaPoalelungi2003 <158208036+MariaPoalelungi2003@users.noreply.github.com>
2024-09-26 17:11:19 +0300

    problem get total18

--- app/(dashboard)/[storeId]/(routes)/colors/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -1,38 +1,39 @@
 import { format } from "date-fns";
-
 import prismadb from "@/lib/prismadb";
 import { ColorsClient } from "./components/client";
-import {ColorColumn} from "./components/columns";
+import { ColorColumn } from "./components/columns";
 
 const ColorsPage = async ({
-    params
-}: {
-    params: {storeId: string}
+                              params,
+                          }: {
+    params: { storeId: string };
 }) => {
     const colors = await prismadb.color.findMany({
         where: {
-            storeId: params.storeId
+            storeId: params.storeId,
         },
-
         orderBy: {
-            createdAt: 'desc'
-        }
+            createdAt: "desc",
+        },
     });
-    
-    const formattedColors: ColorColumn[] = colors.map(( item ) => ({
-        id: item.id,
-        name: item.name,
-        value: item.value,
-        createdAt: format(item.createdAt, "MMMM do, yyyy")
-    }))
+
+    // Adăugarea tipului explicit pentru 'item'
+    const formattedColors: ColorColumn[] = colors.map(
+        (item: { id: string; name: string; value: string; createdAt: Date }) => ({
+            id: item.id,
+            name: item.name,
+            value: item.value,
+            createdAt: format(item.createdAt, "MMMM do, yyyy"),
+        })
+    );
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <ColorsClient data={formattedColors}/>
+                <ColorsClient data={formattedColors} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default ColorsPage;
